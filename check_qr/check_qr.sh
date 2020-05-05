3a58e3c5c642e1ca82a52a7f47018c8398bfd6c9
Marc Dierksen <m.dierksen@netcon-consulting.com>
2020-05-05 14:19:04 +0200

    For 'check_qr', also detect bitcoin walltet IDs and check URLs for IP addresses. For redirections check blacklists for target URL.

--- check_qr/check_qr.sh
+++ check_qr/check_qr.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# check_qr.sh V1.16.0
+# check_qr.sh V1.20.0
 #
-# Copyright (c) 2019 NetCon Unternehmensberatung GmbH, netcon-consulting.com
+# Copyright (c) 2019-2020 NetCon Unternehmensberatung GmbH, https://www.netcon-consulting.com
 #
-# Author: Marc Dierksen (m.dierksen@netcon-consulting.com)
+# Authors:
+# Marc Dierksen (m.dierksen@netcon-consulting.com)
+# Spyros Delimpasis (sdelimpasis@inter-datasecurity.com)
 
 # return codes:
 # 0 - picture does contain no QR code with URL or URL domain is on custom whitelist or not listed on online blacklist
@@ -48,7 +50,13 @@ fi
 RESULT="$(zbarimg "$1" 2>/dev/null)"
 
 if [ "$?" = 0 ] && echo "$RESULT" | grep -q '^QR-Code:'; then
-    LIST_URL="$(echo $RESULT | tr '[:upper:]' '[:lower:]' | awk '{pattern="((https?://|www.|ftp.)[^ ]+)"; while (match($0, pattern, arr)) {val = arr[1]; print val; sub(pattern, "")}}')"
+	# Check if content matches bitcoin wallet ID pattern
+	if  echo "$RESULT" | egrep -q --regexp='[13][a-km-zA-HJ-NP-Z1-9]{25,34}$'; then
+		write_log "Bitcoin address detected"
+		exit 1
+	fi
+
+    LIST_URL="$(echo $RESULT | awk '{pattern="((https?://|www.|ftp.)[^ ]+)"; while (match($0, pattern, arr)) {val = arr[1]; print val; sub(pattern, "")}}')"
 
     if ! [ -z "$LIST_URL" ]; then
         NAME_DOMAIN="$(echo "$URL" | awk 'match($0, /(https?:\/\/)?([^ \/]+)/, a) {print a[2]}')"
@@ -60,7 +68,21 @@ if [ "$?" = 0 ] && echo "$RESULT" | grep -q '^QR-Code:'; then
         [ -z "$FILE_BLACK" ] || LIST_BLACK="$(xmlstarlet sel -t -m "UrlList/Url" -v . -n "$FILE_BLACK")"
 
         for URL in $LIST_URL; do
-            NAME_DOMAIN="$(echo "$URL" | awk 'match($0, /(https?:\/\/)?([^ \/]+)/, a) {print a[2]}')"
+			# Check if URL contains IP address
+			if  echo "$URL" | egrep -q --regexp='(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'; then
+				write_log "Link with IP address instead of domain name"
+				exit 1
+			fi
+
+			# Check if URL redirects to another URL. Target URL is used for lookups
+			REDIRECTION="$(curl -sLI "$URL" | grep -i 'location' | sed 's/^.*: //')"
+
+			if ! [ -z "$REDIRECTION" ] && [ "$?" = 0 ] ; then
+				NAME_DOMAIN="$(echo "$REDIRECTION" | tr '[:upper:]' '[:lower:]' | awk 'match($0, /(https?:\/\/)?([^ \/]+)/, a) {print a[2]}')"
+			else
+				NAME_DOMAIN="$(echo "$URL" | tr '[:upper:]' '[:lower:]'| awk 'match($0, /(https?:\/\/)?([^ \/]+)/, a) {print a[2]}')"
+			fi
+
             DOMAIN_PARTIAL="$NAME_DOMAIN"
             DOMAIN_WHITELISTED=0
 
@@ -79,7 +101,7 @@ if [ "$?" = 0 ] && echo "$RESULT" | grep -q '^QR-Code:'; then
             done
 
             if [ "$DOMAIN_WHITELISTED" != 1 ]; then
-                RESULT="$(dig +short $NAME_DOMAIN.dnsbl7.mailshell.net 2>/dev/null)"
+                RESULT="$(dig +short "$NAME_DOMAIN.dnsbl7.mailshell.net" 2>/dev/null)"
 
                 if [ "$?" = 0 ]; then
                     RESULT="$(echo "$RESULT" | awk -F. '{print $4}')"
@@ -91,7 +113,7 @@ if [ "$?" = 0 ] && echo "$RESULT" | grep -q '^QR-Code:'; then
                 fi
 
                 for BLACKLIST in $LIST_MULTI; do
-                    RESULT="$(dig txt +short $NAME_DOMAIN.multi.$BLACKLIST 2>/dev/null)"
+                    RESULT="$(dig txt +short "$NAME_DOMAIN.multi.$BLACKLIST" 2>/dev/null)"
 
                     if [ "$?" = 0 ] && ! [ -z "$RESULT" ] && ! echo "$RESULT" | grep -q 'Query Refused'; then
                         write_log "$URL [$BLACKLIST]"
