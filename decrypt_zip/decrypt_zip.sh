44e246552ab9e14b91c836bbf3af17b7e50f74ad
Marc Dierksen <m.dierksen@netcon-consulting.com>
2020-05-13 10:54:58 +0200

    For 'decrypt_zip', bugfixes.

--- decrypt_zip/decrypt_zip.sh
+++ decrypt_zip/decrypt_zip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# decrypt_zip.sh V1.2.0
+# decrypt_zip.sh V1.3.0
 #
-# Copyright (c) 2019 NetCon Unternehmensberatung GmbH, netcon-consulting.com
+# Copyright (c) 2019-2020 NetCon Unternehmensberatung GmbH, netcon-consulting.com
 #
 # Author: Marc Dierksen (m.dierksen@netcon-consulting.com)
 
@@ -16,7 +16,7 @@ FILE_PASSWORD='/opt/cs-gateway/scripts/netcon/zip_passwords.txt'
 
 DIR_EXTRACT='/tmp/TMPextract'
 
-FILE_DEBUG='/var/log/decrypt_zip.log'
+FILE_DEBUG='/tmp/decrypt_zip.log'
 
 LOG_PREFIX='>>>>'
 LOG_SUFFIX='<<<<'
@@ -62,20 +62,28 @@ rm -rf "$DIR_EXTRACT"
 mkdir -p "$DIR_EXTRACT"
 
 COUNTER=1
+
 while read -r PASSWORD; do
-    RESULT_EXTRACT="$(unzip -P "$PASSWORD" -d "$DIR_EXTRACT" "$1" 2>&1)"
+    RESULT_EXTRACT="$(yes | unzip -P "$PASSWORD" -d "$DIR_EXTRACT" "$1" 2>&1)"
+
     if [ "$?" = 0 ] && ! echo "$RESULT_EXTRACT" | grep -q 'incorrect password'; then
         VIRUS_FOUND=''
-        for FILE in $(find "$DIR_EXTRACT" -type f -exec echo {} \;); do
+
+        while read FILE; do
             write_debug "Scanning '$FILE'"
+
             if /opt/cs-gateway/bin/sophos/savtest -v -f "$FILE" 2>/dev/null | grep -q VIRUS; then
                 VIRUS_FOUND=1
+
                 write_debug "Virus found in '$FILE'"
                 break
             fi
-        done
+        done < <(find "$DIR_EXTRACT" -type f -exec echo {} \;)
+
         write_log "password $COUNTER"
+
         rm -rf "$DIR_EXTRACT"
+
         if [ "$VIRUS_FOUND" = 1 ]; then
             write_debug "File '$1' successfully decrypted with password $COUNTER but virus found"
             exit 1
@@ -84,9 +92,12 @@ while read -r PASSWORD; do
             exit 0
         fi
     fi
-    COUNTER="$(expr $COUNTER + 1)"
+
+    COUNTER="$(expr "$COUNTER" + 1)"
 done < "$FILE_PASSWORD"
 
 write_debug "File '$1' cannot be decrypted"
+
 rm -rf "$DIR_EXTRACT"
+
 exit 2
