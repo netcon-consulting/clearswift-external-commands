4a7fc484b152f85c864c5b9ad67aea2c1c592dbe
Marc Dierksen <m.dierksen@netcon-consulting.com>
2021-02-18 15:45:16 +0100

    For 'Decrypt ZIP', added options for Kaspersky and Avira AV scanners and removing the encryption.

--- decrypt_zip/README.md
+++ decrypt_zip/README.md
@@ -1,14 +1,17 @@
-decrypt_zip.py V1.0.0
+decrypt_zip.py V2.0.0
 =====================
 
-Attempts to decrypt zip file using a provided list of passwords and if successful scans contents with Sophos AV.
+Attempts to decrypt ZIP container using a provided list of passwords and optionally scans contents with AV and removes encryption.
 
 ## Parameters
 * name_expression_list (string): name of lexical expression list with passwords
+* scan_sophos (boolean): scan contents with Sophos AV
+* scan_kaspersky (boolean): scan contents with Kaspersky AV
+* scan_avira (boolean): scan contents with Avira AV
+* remove_encryption (boolean): remove encryption from ZIP container
 
 ## Lexical expression lists
 * Decrypt ZIP passwords: list of passwords
 
 ## Hold Areas
-* ZIP Virus: mails with zip files containing a virus
-* ZIP decryption failed: mails with zip files that cannot be decrypted with any of the provided passwords
+* Decrypt ZIP: mails with ZIP containers that cannot be decrypted with any of the provided passwords or contain a virus
--- decrypt_zip/decrypt_zip.py
+++ decrypt_zip/decrypt_zip.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# decrypt_zip.py V1.0.0
+# decrypt_zip.py V2.0.0
 #
 # Copyright (c) 2021 NetCon Unternehmensberatung GmbH, https://www.netcon-consulting.com
 # Author: Marc Dierksen (m.dierksen@netcon-consulting.com)
@@ -12,14 +12,13 @@ import sys
 
 import re
 from pathlib import Path
+from io import BytesIO
 from tempfile import TemporaryDirectory
 from subprocess import run, PIPE, DEVNULL
 import pyzipper
-from netcon import ParserArgs, get_config, write_log, get_expression_list, CHARSET_UTF8
+from netcon import ParserArgs, get_config, write_log, get_expression_list, scan_sophos, scan_kaspersky, scan_avira, CHARSET_UTF8
 
-DESCRIPTION = "attempts to decrypt zip file using a provided list of passwords and if successful scans contents with Sophos AV"
-
-SOPHOS_EXECUTABLE = '/opt/cs-gateway/bin/sophos/savfiletest'
+DESCRIPTION = "attempts to decrypt ZIP container using a provided list of passwords and optionally scans contents with AV and removes encryption"
 
 @enum.unique
 class ReturnCode(enum.IntEnum):
@@ -27,18 +26,18 @@ class ReturnCode(enum.IntEnum):
     Return code.
 
     0   - decryption successful
-    1   - decryption successful but virus found
-    2   - decryption failed
+    1   - decryption failed or virus found
+    2   - encryption successfully removed
     99  - error
     255 - unhandled exception
     """
     SUCCESS = 0
-    VIRUS = 1
-    FAILED = 2
+    PROBLEM = 1
+    DECRYPTED = 2
     ERROR = 99
     EXCEPTION = 255
 
-CONFIG_PARAMETERS = ( "name_expression_list", )
+CONFIG_PARAMETERS = ( "name_expression_list", "scan_sophos", "scan_kaspersky", "scan_avira", "remove_encryption" )
 
 def main(args):
     try:
@@ -60,12 +59,33 @@ def main(args):
 
         return ReturnCode.ERROR
 
+    list_scan = list()
+
+    if config.scan_sophos:
+        list_scan.append(scan_sophos)
+
+    if config.scan_kaspersky:
+        list_scan.append(scan_kaspersky)
+
+    if config.scan_avira:
+        list_scan.append(scan_avira)
+
+    virus_found = None
+
+    if config.remove_encryption:
+        buffer = BytesIO()
+
+        zf_decrypted = pyzipper.AESZipFile(buffer, "w", compression=pyzipper.ZIP_LZMA)
+
     with TemporaryDirectory(dir="/tmp") as path_tmpdir:
         path_tmpdir = Path(path_tmpdir)
 
-        for password in set_password:
-            try:
-                with pyzipper.AESZipFile(args.input, "r", compression=pyzipper.ZIP_LZMA, encryption=pyzipper.WZ_AES) as zf:
+        if config.scan_avira:
+            path_tmpdir.chmod(0o755)
+
+        with pyzipper.AESZipFile(args.input, "r", compression=pyzipper.ZIP_LZMA, encryption=pyzipper.WZ_AES) as zf:
+            for password in set_password:
+                try:
                     zf.pwd = password.encode(CHARSET_UTF8)
 
                     for file_name in zf.namelist():
@@ -81,25 +101,44 @@ def main(args):
 
                             return ReturnCode.ERROR
 
-                        try:
-                            result = run([ SOPHOS_EXECUTABLE, "-v", "-f", path_file ], check=True, stdout=PIPE, stderr=DEVNULL, encoding=CHARSET_UTF8)
-                        except:
-                            write_log(args.log, "Error calling Sophos AV")
+                        path_file = str(path_file)
 
-                            return ReturnCode.ERROR
+                        for scan in list_scan:
+                            try:
+                                virus_found = scan(path_file)
+                            except Exception as ex:
+                                write_log(args.log, ex)
 
-                        match = re.search(r"\n\tSophosConnection::recvLine returning VIRUS (\S+) {}\n".format(path_file), result.stdout)
+                                return ReturnCode.ERROR
 
-                        if match:
-                            write_log(args.log, match.group(1))
+                            if virus_found is not None:
+                                break
 
-                            return ReturnCode.VIRUS
+                        if virus_found is not None:
+                            write_log(args.log, "Virus '{}'".format(virus_found))
 
-                    return ReturnCode.SUCCESS
-            except RuntimeError:
-                pass
+                            return ReturnCode.PROBLEM
 
-    return ReturnCode.FAILED
+                        if config.remove_encryption:
+                            zf_decrypted.writestr(file_name, data)
+
+                    break
+                except RuntimeError:
+                    pass
+            else:
+                write_log(args.log, "Decryption failed")
+
+                return ReturnCode.PROBLEM
+
+    if config.remove_encryption:
+        zf_decrypted.close()
+
+        with open(args.input, "wb") as f:
+            f.write(buffer.getvalue())
+
+        return ReturnCode.DECRYPTED
+
+    return ReturnCode.SUCCESS
 
 #########################################################################################
 
