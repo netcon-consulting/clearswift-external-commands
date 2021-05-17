23d9c824966adafeab63998ef1183f0fc35064f1
Marc Dierksen <m.dierksen@netcon-consulting.com>
2021-05-17 16:05:00 +0200

    For 'check_qr', bugfix.

--- check_qr/check_qr.py
+++ check_qr/check_qr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# check_qr.py V1.0.0
+# check_qr.py V1.0.1
 #
 # Copyright (c) 2021 NetCon Unternehmensberatung GmbH, https://www.netcon-consulting.com
 # Author: Marc Dierksen (m.dierksen@netcon-consulting.com)
@@ -13,7 +13,7 @@ import sys
 import re
 from PIL import Image
 from pyzbar.pyzbar import decode
-from netcon import ParserArgs, get_config, write_log, get_url_list, domain_blacklisted, CHARSET_UTF8
+from netcon import ParserArgs, get_config, write_log, get_url_list, domain_blacklisted, url2regex, CHARSET_UTF8
 
 DESCRIPTION = "check URLs from QR-codes in pictures against URL blacklist and corresponding domains against reputation blacklists"
 
@@ -47,7 +47,7 @@ def main(args):
 
     try:
         image = Image.open(args.input)
-    except:
+    except Exception:
         write_log(args.log, "Cannot read image '{}'".format(args.input))
 
         return ReturnCode.ERROR
@@ -57,17 +57,27 @@ def main(args):
     for qr_code in list_qr:
         try:
             text = qr_code.data.decode(CHARSET_UTF8)
-        except:
+        except Exception:
             text = None
 
         if text is not None and re.search(PATTERN_URL, text) is not None:
             try:
-                set_blacklist = { re.compile(r"^{}$".format(url.replace(".", r"\.").replace("*", ".*"))) for url in get_url_list(config.name_url_blacklist) }
-                set_whitelist = { re.compile(r"^{}$".format(url.replace(".", r"\.").replace("*", ".*"))) for url in get_url_list(config.name_url_whitelist) }
+                set_url = get_url_list(config.name_url_blacklist)
             except Exception as ex:
                 write_log(args.log, ex)
 
                 return ReturnCode.ERROR
+
+            set_blacklist = { re.compile(url2regex(url), re.IGNORECASE) for url in set_url }
+
+            try:
+                set_url = get_url_list(config.name_url_whitelist)
+            except Exception as ex:
+                write_log(args.log, ex)
+
+                return ReturnCode.ERROR
+
+            set_whitelist = { re.compile(url2regex(url), re.IGNORECASE) for url in set_url }
 
             for match in re.finditer(PATTERN_URL, text):
                 url = match.group()
@@ -78,7 +88,7 @@ def main(args):
                 else:
                     for pattern in set_blacklist:
                         if re.search(pattern, url) is not None:
-                            write_log(args.log, "'{}' listed on '{}'".format(url, args.name_url_blacklist))
+                            write_log(args.log, "'{}' listed on '{}'".format(url, config.name_url_blacklist))
 
                             return ReturnCode.MALICIOUS
 
